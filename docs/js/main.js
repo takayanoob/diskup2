5638d62ac1ade6bfefad7f01925e956ca48e491e
takayanoob <takayan1994@gmail.com>
2022-05-21 01:16:36 +0900

    チェック関係更新
    ・チェックが入ったら済を重ねて表示
    ・変更昨日のON/OFFを追加

--- docs/js/main.js
+++ docs/js/main.js
@@ -18,18 +18,22 @@ $(document).ready(function() {
     for(i = 1; i<= IMAGE_MAX; i++){
         //  チェック状態確認
         let checked_txt = "";
+        let checked_cls = "";
         if (i in clear_check){
             //  配列に存在する
             if(clear_check[i] === true){
                 checked_txt = "checked";
+                checked_cls = "check";
             }
         }
         let number = ( '000' + i ).slice( -3 );
-        let image_box = "<li>";
+        let image_box = "<li class=\"img_box " + checked_cls + "\">";
         image_box += "<input id=\"clear_" + number + "\" type=\"checkbox\" class=\"clear_check\" name=\"clear_check\" value=\""+String(i)+"\" " + checked_txt + ">";
         image_box += "<label for=\"clear_" + number + "\">";
         image_box += "<img src=\"./img/"+number+".jpg\" alt=\""+number+"\">";
         image_box += "</label>";
+        // image_box += "<span class=\"img_over\">済</span>";
+        image_box += "<img class=\"img_over\" src=\"./img/work/sumi.png\" alt=\"済\">";
         image_box += "</li>";
         $('ul.image_table').append(image_box);
     }
@@ -37,6 +41,7 @@ $(document).ready(function() {
     //  カウント最大数の更新
     $('.max_collection').text( String(IMAGE_MAX) );
 
+    
     //  チェックされた数のカウント
     $(document.body).on('change','.clear_check',function(event){
         //  チェック数の更新
@@ -54,6 +59,24 @@ $(document).ready(function() {
 
     //  初回のチェック数更新
     clear_check_cnt_update();
+
+    //  チェックボックスにチェックが入ったら親にcheckクラスを渡す
+    $('.img_box input.clear_check').on('change',function(){
+        if($(this).prop('checked')){
+            $(this).parent('.img_box').addClass('check');
+        }else{
+            $(this).parent('.img_box').removeClass('check');
+        }
+    });
+
+    //  編集ON/OFFボタン
+    $(document.body).on('change','.switchArea #switch1',function(event){
+        if($(this).prop('checked')){
+            $('.img_box').removeClass('not_edit');
+        }else{
+            $('.img_box').addClass('not_edit');
+        }
+    });
 });
 
 //  チェック数の更新
